5f7766a7528f1f932d78a95df77c1a3227b3ba36
Cory Cowden <corycowden@Corys-MacBook-Air.local>
2025-12-09 17:15:36 -0800

    employee instance of itself

--- src/main/java/org/example/Employee.java
+++ src/main/java/org/example/Employee.java
@@ -2,6 +2,7 @@ package org.example;
 
 public class Employee extends Person { // Inheritance
     private Company employer; // Association
+    private static Employee instance;
 
     public Employee(String name, Address address, Company employer) {
         super(name, address);
